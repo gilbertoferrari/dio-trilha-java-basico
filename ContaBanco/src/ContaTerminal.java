627f46dc93b0c91ecab3cfb1d896a54a97d446ac
gilbertoferrari <gilbertoferrari21@gmail.com>
2025-06-20 15:00:15 -0300

    criado a classe ContaTerminal e suas variavéis

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome!");
+            String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o numero da agencia!");
+            String agencia = scanner.nextLine();
+
+        System.out.println("Digite o numero da conta!");
+            int numero = scanner.nextInt();
+
+        System.out.println("Digite seu saldo!");
+            double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + "e seu saldo " + saldo + " já está disponivel para saque.");
+
+        scanner.close();
     }
 }
